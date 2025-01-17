8145de760ce03ca3ae847e9bdd58085085316a65
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-01-17 16:25:51 +0100

    additional logging in protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,8 +1,9 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated } = useAuth();
+  console.log("isAuthenticated from ProtectedRoute", isAuthenticated);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
